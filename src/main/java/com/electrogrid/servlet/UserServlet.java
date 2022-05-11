71f219cc9999e88978f6e1bded1ba1e8b901b243
Ranasinghe TKSA <sachin.ranasinghe1998.13@gmail.com>
2022-05-11 20:31:37 +0530

    routing added

--- src/main/java/com/electrogrid/servlet/UserServlet.java
+++ src/main/java/com/electrogrid/servlet/UserServlet.java
@@ -2,15 +2,20 @@ package com.electrogrid.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.electrogrid.model.User;
 
+@WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	User userObject = new User();
 
     public UserServlet() {
+    	super();
         // TODO Auto-generated constructor stub
     }
 
